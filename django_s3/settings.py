aa37228c8942208eb7d2dea092ab397dc3abb163
Alex Clark <aclark@aclark.net>
2015-06-10 19:48:18 -0400

    Configure IAM

--- django_s3/settings.py
+++ django_s3/settings.py
@@ -101,3 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# AWS Identity and Access Management (IAM)
+# http://aws.amazon.com/iam/ 
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID', '')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY', '')
