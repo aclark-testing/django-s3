5debbd8e5790223b6608ee815f1f940c7031531c
Alex Clark <aclark@aclark.net>
2015-06-12 18:37:36 -0400

    Upload to s3

--- django_s3/django_s3/views.py
+++ django_s3/django_s3/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django_s3.django_s3.forms import UploadFileForm
 from django_s3.django_s3.utils import s3_connect
+from django_s3.settings import AWS_S3_BUCKET_ID
 
 
 def home(request):
@@ -14,7 +15,15 @@ def home(request):
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # ...
+
+            # upload to s3
+            from boto.s3.key import Key
             conn = s3_connect()
+            bucket = conn.get_bucket(AWS_S3_BUCKET_ID)
+            key = Key(bucket)
+            upload_file = request.FILES['upload_file']
+            key.key = upload_file.name
+            key.set_contents_from_file(upload_file)
 
             # redirect to a new URL:
             return HttpResponseRedirect('/')
--- django_s3/settings.py
+++ django_s3/settings.py
@@ -106,3 +106,4 @@ STATIC_URL = '/static/'
 # http://aws.amazon.com/iam/ 
 AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+AWS_S3_BUCKET_ID=os.environ.get('AWS_S3_BUCKET_ID', '')
